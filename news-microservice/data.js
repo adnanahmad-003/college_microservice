a47e3b2d1cf5f5666e5e384c511450a130449e26
Sithartha S G <sitharthasg@gmail.com>
2024-11-16 15:58:00 +0530

    Update data.js

--- news-microservice/data.js
+++ news-microservice/data.js
@@ -1,5 +1,61 @@
 module.exports = [
-    { id: 1, title: "Hackathon 2024", description: "Join our college's annual hackathon." },
-    { id: 2, title: "Convocation 2024", description: "Save the date for the convocation." },
-    { id: 3, title: "AI Workshop", description: "Learn the latest in AI this semester." }
+    { 
+        id: 1, 
+        title: "Inter IIIT Sports Meet 2024", 
+        startDate: "06-03-2024", 
+        endDate: "16-03-2024", 
+        organiser: "IIIT Allahabad", 
+        venue: {
+            place: "IIIT Allahabad",
+            city: "Prayagraj"
+        }, 
+        description: "Inter IIIT, held every four years by IIIT Allahabad, unites athletes from 25 government-funded IIITs in a premier sports competition. Celebrating sportsmanship and fostering lasting bonds, it is a key event in the IIIT community."
+    },
+    { 
+        id: 2, 
+        title: "78th Independence Day Celebration", 
+        startDate: "15-08-2024", 
+        endDate: "15-08-2024", 
+        organiser: "IIIT Dharwad", 
+        venue: {
+            place: "IIIT Dharwad",
+            city: "Dharwad"
+        }, 
+        description: "Celebration of the 78th Independence Day."
+    },
+    { 
+        id: 3, 
+        title: "Inauguration of IIIT Dharwad", 
+        date: "26-09-2022", 
+        organiser: "IIIT Dharwad", 
+        venue: {
+            place: "M Block",
+            street: "IIIT Dharwad Campus",
+            city: "Dharwad"
+        }, 
+        description: "Inauguration of IIIT Dharwad on 26-09-2022."
+    },
+    { 
+        id: 4, 
+        title: "World Blood Donor Day 2022", 
+        date: "14-06-2022", 
+        organiser: "IIIT Dharwad", 
+        venue: {
+            place: "H-Block",
+            street: "IIIT Campus",
+            city: "Dharwad"
+        }, 
+        description: "Sir Karl Landsteiner's Birthday celebrated as World Blood Donors Day in coordination with Karnataka Cancer Therapy and Research Institute, Navanagar."
+    },
+    { 
+        id: 5, 
+        title: "World Environment Day 2022", 
+        date: "05-06-2022", 
+        organiser: "IIIT Dharwad", 
+        venue: {
+            place: "IIIT Dharwad",
+            city: "Dharwad"
+        }, 
+        description: "Celebration of World Environment Day at IIIT Dharwad."
+    }
 ];
